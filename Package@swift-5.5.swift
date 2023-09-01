c8abb295486fd6eb634da218a6fa965a6b3dd4fe
Baptiste C <baptistecdr@users.noreply.github.com>
2023-09-01 06:51:47 +0200

    Update Alamofire

--- Package@swift-5.5.swift
+++ Package@swift-5.5.swift
@@ -15,7 +15,7 @@ let package = Package(
                     targets: ["Aria2Kit"]),
         ],
         dependencies: [
-            .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire", .revision("5.7.1")),
+            .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire", .revision("5.8.0")),
             .package(name: "AnyCodable", url: "https://github.com/Flight-School/AnyCodable", .revision("0.6.7")),
         ],
         targets: [
